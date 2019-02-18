e6eccb86a8d83e00b8558a465a26bc0d36d214fc
Fenniless <fenniless@gmail.com>
2019-02-18 13:58:40 -0500

    3 parts done

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -1,8 +1,45 @@
 package com.zipcodewilmington.assessment1.part5;
 
+import java.util.Arrays;
+
+import static jdk.nashorn.internal.objects.NativeArray.reverse;
+
 public class Palindrome {
 
+    public static String reverse(String valueToBeReversed) {
+        StringBuilder input1 = new StringBuilder();
+        input1.append(valueToBeReversed);
+        input1 = input1.reverse();
+
+        return input1.toString();
+    }
+
+    public static boolean isPalindromic(String str) {
+        boolean pal = false;
+        if (reverse(str).equals(str)){
+            pal = true;
+        }
+        return pal;
+    }
+
     public Integer countPalindromes(String input){
-        return null;
+
+        int numOfPals = 0;
+        int inputLength = input.length();
+        int maxSubDiv = input.length()/2;
+//        if(isPalindromic(input)){
+//            numOfPals++;
+//        }
+        for(int i = 0 ; i < inputLength ; i++){
+            for(int j = inputLength; i < j ; j--){
+                if(isPalindromic(input.substring(i,j))){
+                numOfPals++;
+            }
+
+
+        }}
+
+
+        return numOfPals;
     }
 }
