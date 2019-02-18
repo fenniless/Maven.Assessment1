1429a162f77bd18a3db7d67639b043e5aa911a66
Fenniless <fenniless@gmail.com>
2019-02-18 15:54:54 -0500

    a few tests left

--- src/main/java/com/zipcodewilmington/assessment1/part3/PetOwner.java
+++ src/main/java/com/zipcodewilmington/assessment1/part3/PetOwner.java
@@ -1,27 +1,38 @@
 package com.zipcodewilmington.assessment1.part3;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by leon on 2/16/18.
  */
 public class PetOwner {
+    private String name;
+    private ArrayList<Pet> pets = new ArrayList<>();
+
+
     /**
      * @param name name of the owner of the Pet
      * @param pets array of Pet object
      */
     public PetOwner(String name, Pet... pets) {
+        super();
+
     }
 
     /**
      * @param pet pet to be added to the composite collection of Pets
      */
     public void addPet(Pet pet) {
+        pets.add(pet);
     }
 
     /**
      * @param pet pet to be removed from the composite collection Pets
      */
     public void removePet(Pet pet) {
-
+    pets.remove(pet);
     }
 
     /**
@@ -29,6 +40,7 @@ public class PetOwner {
      * @return true if I own this pet
      */
     public Boolean isOwnerOf(Pet pet) {
+
         return null;
     }
 
@@ -36,6 +48,7 @@ public class PetOwner {
      * @return the age of the Pet object whose age field is the lowest amongst all Pets in this class
      */
     public Integer getYoungetPetAge() {
+
         return null;
     }
 
@@ -46,6 +59,7 @@ public class PetOwner {
      * @return the age of the Pet object whose age field is the highest amongst all Pets in this class
      */
     public Integer getOldestPetAge() {
+
         return null;
     }
 
@@ -54,6 +68,7 @@ public class PetOwner {
      * @return the sum of ages of Pet objects stored in this class divided by the number of Pet object
      */
     public Float getAveragePetAge() {
+
         return null;
     }
 
@@ -61,6 +76,7 @@ public class PetOwner {
      * @return the number of Pet objects stored in this class
      */
     public Integer getNumberOfPets() {
+
         return null;
     }
 
@@ -68,6 +84,7 @@ public class PetOwner {
      * @return the name property of the Pet
      */
     public String getName() {
+
         return null;
     }
 
@@ -75,6 +92,7 @@ public class PetOwner {
      * @return array representation of animals owned by this PetOwner
      */
     public Pet[] getPets() {
+
         return null;
     }
 }
